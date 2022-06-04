600e5391165d2e6fd34899d415db9365c048ce22
MangayaDaryl <94291361+MangayaDaryl@users.noreply.github.com>
2022-06-05 02:57:34 +0800

    Add files via upload

---
+++ MainPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PDC03_MOD2
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Button btn = new Button();
+            btn.Text = "Click Me!";
+            btn.TextColor = Color.Olive;
+            btn.BackgroundColor = Color.Accent;
+            btn.FontSize = 25;
+            btn.Clicked += Btn_Clicked;
+            Content = new StackLayout
+            {
+                Children = {btn }
+            };
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Second());
+        }
+    }
+}
---
+++ Second.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PDC03_MOD2
+{
+    public class Second : ContentPage
+    {
+        public Second()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
